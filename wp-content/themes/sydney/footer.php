37f627bf613df3a9b5841d193ed4044820a38ca7
OjTouch <ojtouch@gmail.com>
2019-02-27 18:25:46 +0100

    contact form added

--- wp-content/themes/sydney/footer.php
+++ wp-content/themes/sydney/footer.php
@@ -3,8 +3,6 @@
  * The template for displaying the footer.
  *
  * Contains the closing of the #content div and all content after
- *
- * @package Sydney
  */
 ?>
 			</div>
@@ -13,7 +11,7 @@
 
 	<?php do_action('sydney_before_footer'); ?>
 
-	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+	<?php if (is_active_sidebar('footer-1')) : ?>
 		<?php get_sidebar('footer'); ?>
 	<?php endif; ?>
 
@@ -21,9 +19,7 @@
 		
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sydney' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'sydney' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %2$s by %1$s.', 'sydney' ), 'aThemes', '<a href="https://athemes.com/theme/sydney" rel="designer">Sydney</a>' ); ?>
+			Copyright 2019 - Chalets & Caviar
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
